ab25d0339a0cca4ee91943d9813d5e9c9c198719
v1ckyfrfr <vickyyrachman@gmail.com>
2026-07-22 21:24:55 +0700

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,12 @@
-"use client";
-
-import { useState } from "react";
-
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
 import { Footer } from "@/components/Footer";
-
-import { StrukturSection } from "@/components/sections/StrukturSection";
-import { PiketSection } from "@/components/sections/PiketSection";
-import { GaleriSection } from "@/components/sections/GaleriSection";
+import { StrukturSection, PiketSection, GaleriSection } from "./lazy-sections";
 
 export default function Page() {
-  const [activeNav, setActiveNav] = useState("Beranda");
-
   return (
     <main>
-      <Navbar active={activeNav} setActive={setActiveNav} />
+      <Navbar />
       <Hero />
       <StrukturSection />
       <PiketSection />
