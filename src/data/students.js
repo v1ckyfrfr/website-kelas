a34d1f397d00a5037d6451f1954d766899d0589e
v1ckyfrfr <vickyyrachman@gmail.com>
2026-07-25 22:13:42 +0700

    update data

--- src/data/students.js
+++ src/data/students.js
@@ -5,36 +5,82 @@ export const STUDENTS = [
     icon: "crown",
     color: "#ff6eb4",
   },
+
   {
     name: "Annisa Nur Rohmah",
     role: "Wakil Ketua",
     icon: "star",
     color: "#ff85c2",
   },
-  { name: "Lyana W.", role: "Sekretaris 1", icon: "edit", color: "#ffadd6" },
-  { name: "Glorya A.", role: "Sekretaris 2", icon: "edit", color: "#ffc4e3" },
-  { name: "Reyhan", role: "Keamanan 1", icon: "shield", color: "#ff6eb4" },
+
+  { 
+    name: "Lyana W.", 
+    role: "Sekretaris 1", 
+    icon: "edit", 
+    color: "#ffadd6" 
+  },
+
+  { name: "Glorya A.", 
+    role: "Sekretaris 2", 
+    icon: "edit", 
+    color: "#ffc4e3" 
+  },
+
+  { name: "Reyhan", 
+    role: "Keamanan 1", 
+    photo: "/students/rehan.png",
+    color: "#ff6eb4" 
+  },
+
   {
     name: "Naofal Dwi S.",
     role: "Keamanan 2",
-    icon: "shield",
+    photo: "/students/naofal.png",
     color: "#ff85c2",
   },
-  { name: "Amanda P.", role: "Bendahara 1", icon: "coins", color: "#ffadd6" },
-  { name: "Velita S.", role: "Bendahara 2", icon: "coins", color: "#ffc4e3" },
+
+  { name: "Amanda P.", 
+    role: "Bendahara 1", 
+    icon: "coins", 
+    color: "#ffadd6" 
+  },
+
+  { name: "Velita S.", 
+    role: "Bendahara 2", 
+    icon: "coins", 
+    color: "#ffc4e3" 
+  },
+
   {
     name: "Vicky R.",
     role: "Anggota & Developer",
-    photo: "/students/vicky.webp",
+    photo: "/students/vickyR.png",
     color: "#ff6eb4",
   },
-  { name: "Khalif A. F.", role: "Anggota", icon: "fire", color: "#ff85c2" },
+
+  { 
+    name: "Khalif A. F.", 
+    role: "Anggota", 
+    photo: "/students/khalif.png",
+    color: "#ff85c2" 
+  },
+
   {
     name: "Dirga Achmad P. B.",
     role: "Anggota",
     icon: "star",
     color: "#ffadd6",
   },
-  { name: "Vinza R. A. M.", role: "Anggota", icon: "mask", color: "#ffc4e3" },
-  { name: "Zacky Q.", role: "Anggota", icon: "bolt", color: "#ffadd6" },
+
+  { name: "Vinza R. A. M.", 
+    role: "Anggota", 
+    photo: "/students/vinza.png", 
+    color: "#ffc4e3" 
+  },
+
+  { name: "Zacky Q.", 
+    role: "Anggota", 
+    photo: "/students/zacky.png",
+    color: "#ffadd6" 
+  },
 ];
