6f166baaa1e3135c425b1a739384ca53b23d3a5f
v1ckyfrfr <vickyyrachman@gmail.com>
2026-07-25 22:19:20 +0700

    fixes array structure

--- src/data/piket.js
+++ src/data/piket.js
@@ -1,7 +1,28 @@
 export const PIKET = [
-  { day: "Senin", group: "Kelompok 1", members: "Lyana, Farah, Vicky" },
-  { day: "Selasa", group: "Kelompok 2", members: "Velita, Dirga, Vinza" },
-  { day: "Rabu", group: "Kelompok 3", members: "Reyhan, Glorya, Zacky" },
-  { day: "Kamis", group: "Kelompok 4", members: "Naofal, Amanda" },
-  { day: "Jumat", group: "Kelompok 5", members: "Khalif, Anisa" },
+  { 
+    day: "Senin", 
+    group: "Kelompok 1", 
+    members: "Lyana, Farah, Vicky" 
+  },
+
+  { day: "Selasa", 
+    group: "Kelompok 2", 
+    members: "Velita, Dirga, Vinza" 
+  },
+
+  { 
+    day: "Rabu", 
+    group: "Kelompok 3", 
+    members: "Reyhan, Glorya, Zacky" 
+  },
+
+  { day: "Kamis", 
+    group: "Kelompok 4", 
+    members: "Naofal, Amanda" 
+  },
+  
+  { day: "Jumat", 
+    group: "Kelompok 5", 
+    members: "Khalif, Anisa" 
+  },
 ];
