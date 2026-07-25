65db6faabacf58734688e322363dca37c3242828
v1ckyfrfr <vickyyrachman@gmail.com>
2026-07-25 22:20:41 +0700

    update + fixed array

--- src/data/structure.js
+++ src/data/structure.js
@@ -1,8 +1,37 @@
 export const STRUCTURE = [
-  { id: "wali", label: "Wali Kelas", name: "M. Iqbal B." },
-  { id: "ketua", label: "Ketua Kelas", name: "Farah G." },
-  { id: "wakil", label: "Wakil Ketua", name: "Annisa N. R." },
-  { id: "sek", label: "Sekretaris 1 & 2", name: "Lyana W. - Glorya A." },
-  { id: "ben", label: "Bendahara 1 & 2", name: "Amanda P. - Velita S." },
-  { id: "hum", label: "Keamanan 1 dan 2", name: "Reyhan - Naofal Dwi S." },
+  { 
+    id: "wali", 
+    label: "Wali Kelas", 
+    name: "M. Iqbal B." 
+  },
+
+  { 
+    id: "ketua", 
+    label: "Ketua Kelas", 
+    name: "Farah G." 
+  },
+
+  {
+     id: "wakil", 
+    label: "Wakil Ketua", 
+    name: "Annisa N. R." 
+  },
+
+  {
+     id: "sek", 
+    label: "Sekretaris 1 & 2", 
+    name: "Lyana W. - Glorya A." 
+  },
+
+  {
+     id: "ben", 
+    label: "Bendahara 1 & 2", 
+    name: "Amanda P. - Velita S." 
+  },
+  
+  { 
+    id: "hum", 
+    label: "Keamanan 1 dan 2", 
+    name: "Reyhan - Naofal Dwi S." 
+  },
 ];
